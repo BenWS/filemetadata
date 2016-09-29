5466bc5a1bdde053470a79c4ecf79753cfee9c5e
Ben <shippeyben@gmail.com>
2016-09-29 19:39:17 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+//import express
+var express = require('express');
+var app = express();
+
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
+//set view directory
+app.set("views", ".");
+app.set("view engine", "pug");
+
+app.get('/*', (req, res) => {
+    res.render("index")
+})
+
+app.post('/getFileSize', upload.single("file"),(req, res) => {
+    console.log(req.file.size);
+    res.json({"size in bytes": req.file.size});
+})
+
+app.listen(8080);
+
+// var fileStats = fs.statSync("myFile.txt");
+// console.log(fileStats["size"]);
